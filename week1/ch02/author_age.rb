f6a82a22bb5d0bffa53b26be64c446296d8e30c9
nazgus <gus@deleon.org.mx>
2018-06-13 21:20:59 -0700

    Convert the number to a string

--- week1/ch02/author_age.rb
+++ week1/ch02/author_age.rb
@@ -1,1 +1,1 @@
-puts "I read three books per month, each of ~100k words each, and there's twelve months in a year, so that means that I read " + (3 * 12 * 10**5) + " words per year!"
+puts "I read three books per month, each of ~100k words each, and there's twelve months in a year, so that means that I read " + (3 * 12 * 10**5).to_s + " words per year!"
