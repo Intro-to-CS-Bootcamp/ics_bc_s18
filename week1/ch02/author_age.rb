e94ec30165138cbf74dfe068b6dd9a2e3795015c
nazgus <gus@deleon.org.mx>
2018-06-15 08:36:00 -0700

    Complete week 1 assignments

--- week1/ch02/author_age.rb
+++ week1/ch02/author_age.rb
@@ -1,1 +1,3 @@
 puts "I read three books per month, each of ~100k words each, and there's twelve months in a year, so that means that I read " + (3 * 12 * 10**5).to_s + " words per year!"
+
+puts "blah"
