386cba72ea5fd042bbe83273dad6d91e2980df82
nazgus <nazgus@users.noreply.github.com>
2018-06-17 17:08:41 -0700

    Hard code exit

--- lab_runner.rb
+++ lab_runner.rb
@@ -2,6 +2,14 @@ require "rubygems"
 require "open-uri"
 require "json"
 
+def save_and_exit
+	if $progress_file && $progress
+		File.open($progress_file, "w") { |pf| pf.write($progress.to_s + "\n") }
+		puts "\nWe'll save your progress for next time!"
+	end
+	exit
+end
+
 def expect(answer, correct_msg='OK!')
 	answer = answer.downcase
 	input = flush_and_get.downcase
@@ -23,18 +31,19 @@ end
 
 def flush_and_get
 	$stdout.flush
-	$stdin.gets.chomp
+	input = $stdin.gets.chomp
+	input == 'exit' ? save_and_exit() : input
 end
 
-$valid_lab_start = 1
-$valid_lab_end = 1
+valid_lab_start = 1
+valid_lab_end = 1
 
 if ARGV.length != 1
 	puts 'You must provide a single argument for the number of the lab you want to run.'
 	exit
-elsif ARGV[0].to_i < $valid_lab_start || ARGV[0].to_i > $valid_lab_end
+elsif ARGV[0].to_i < valid_lab_start || ARGV[0].to_i > valid_lab_end
 	puts "That's not a valid lab number, please input a number between " +
-	     "#{$valid_lab_start} and #{$valid_lab_start}."
+	     "#{valid_lab_start} and #{valid_lab_start}."
 	exit
 end
 
@@ -54,14 +63,12 @@ Dir.chdir("week" + ARGV[0])
 encoded_url = URI.encode(File.read("lab" + ARGV[0] + ".txt").chomp)
 url = URI.parse(encoded_url)
 open(url) do |f|
-	progress_file = ".lab" + ARGV[0] + "_progress.txt"
-	progress = File.exist?(progress_file) ? File.read(progress_file).chomp.to_i : 0
+	$progress_file = ".lab" + ARGV[0] + "_progress.txt"
+	$progress = File.exist?($progress_file) ? File.read($progress_file).chomp.to_i : 0
 	offset = chapter_index = 0
 
 	trap("INT") do
-		File.open(progress_file, "w") { |pf| pf.write(progress.to_s + "\n") }
-		puts "\nWe'll save your progress for next time!"
-		exit
+		save_and_exit
 	end
 
 	JSON.parse(f.read).each do |chapter|
@@ -74,16 +81,16 @@ open(url) do |f|
 			chapter_index += 1
 			puts '----------Question ' + chapter_index.to_s + '-----------'
 			question['question'].each { |line| puts line }
-			if (chapter_index + offset) <= progress
+			if (chapter_index + offset) <= $progress
 				puts question['answer']
 			else
 				question["exact"] ? expect_exact(question['answer']) : expect(question['answer'])
-				progress += 1
+				$progress += 1
 			end
 			puts ''
 		end
 		puts ''
 	end
 
-	File.open(progress_file, "w") { |pf| pf.write(progress.to_s + "\n") }
+	save_and_exit()
 end
