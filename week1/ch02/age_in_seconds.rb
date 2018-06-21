e38ec59a78727305f15ced2c837e8d66034741fc
nazgus <gus@deleon.org.mx>
2018-06-20 22:20:45 -0700

    Revert "Merge branch 'master' of https://github.com/Intro-to-CS-Bootcamp/ics_bc_s18"
    This reverts commit 46c9ca3150711843f079a8b8dc8c9a5b35f6e4af, reversing
    changes made to 31c557625d233221983974d5697f8b1779477d2e.

--- week1/ch02/age_in_seconds.rb
+++
@@ -1,3 +0,0 @@
-puts "I read about 4 books per month, and they are on average about 100k words long, so that means that I read about " + (12 * 4 * 10**5).to_s + " words per year!"
-
-puts "blah"
--- week1/ch02/author_age.rb
+++
@@ -1,3 +0,0 @@
-puts "I read three books per month, each of ~100k words each, and there's twelve months in a year, so that means that I read " + (3 * 12 * 10**5).to_s + " words per year!"
-
-puts "blah"
